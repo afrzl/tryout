634bd647ab957fa752bdb4b3471d54c0b3ef9f38
Muh. Nur Afrizal <muhamadnurafrizal5@gmail.com>
2024-03-30 14:07:22 +0700

    fix

--- app/Http/Controllers/UjianController.php
+++ app/Http/Controllers/UjianController.php
@@ -231,9 +231,6 @@ class UjianController extends Controller
         $ujian = Ujian::with('ujianUser')->findOrFail($id);
         return ($ujian);
         $betweenTime = Carbon::now()->between($ujian->waktu_mulai, $ujian->waktu_akhir);
-        if ((!$betweenTime) || ((isset($ujian->ujianUser[0]->status) ? $ujian->ujianUser[0]->status : null) == 2 && $ujian->tipe_ujian == 1)) {
-            abort(403, "Tidak dapat masuk ujian");
-        }
 
         $session = Session::where('user_id', auth()->user()->id)->get();
         if ($session->count() > 1) {

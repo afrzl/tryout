f5b4cc10a0a7c8f0fde20e2e321641399e2c19fd
Muh. Nur Afrizal <muhamadnurafrizal5@gmail.com>
2024-03-30 13:43:14 +0700

    fix masuk ujian

--- app/Http/Controllers/UjianController.php
+++ app/Http/Controllers/UjianController.php
@@ -230,7 +230,7 @@ class UjianController extends Controller
     {
         $ujian = Ujian::findOrFail($id);
         $betweenTime = Carbon::now()->between($ujian->waktu_mulai, $ujian->waktu_akhir);
-        if ((!$betweenTime) || ((isset($ujian->ujianUser[0]->status) ? $ujian->ujianUser[0]->status : null) == 2 && $ujian->tipe_ujian == 1)) {
+        if (!((!$betweenTime) || ((isset($ujian->ujianUser[0]->status) ? $ujian->ujianUser[0]->status : null) == 2 && $ujian->tipe_ujian == 1))) {
             abort(403, "Tidak dapat masuk ujian");
         }
 

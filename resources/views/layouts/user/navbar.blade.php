4a2ff6c35b212f80d002be288733ab196e14b596
Muh. Nur Afrizal <muhamadnurafrizal5@gmail.com>
2024-05-01 21:28:32 +0700

    fix: peletakan

--- resources/views/layouts/user/navbar.blade.php
+++ resources/views/layouts/user/navbar.blade.php
@@ -3,8 +3,8 @@
         <li><a class="nav-link scrollto {{ (request()->segment(1) == '') ? 'active' : '' }}" href="{{ (request()->segment(1) == '') ? '#hero' : route('dashboard') }}">Home</a></li>
         <li><a class="nav-link scrollto" href="{{ (request()->segment(1) == '') ? '#pricing' : route('dashboard') . '/#pricing' }}">Paket Ujian</a></li>
         <li><a class="nav-link scrollto {{ (request()->segment(1) == 'tryout' || request()->segment(2) == 'tryout') ? 'active' : '' }}" href="{{ route('tryout.index') }}">Tryout</a></li>
-        <li><a class="nav-link scrollto {{ (request()->segment(1) == 'pengumuman') ? 'active' : '' }}"  href="{{ route('pengumuman.index') }}">Pengumuman</a></li>
         <li><a class="nav-link scrollto {{ (request()->segment(1) == 'faq') ? 'active' : '' }}"  href="{{ route('faq.index') }}">FAQ</a></li>
+        <li><a class="nav-link scrollto {{ (request()->segment(1) == 'pengumuman') ? 'active' : '' }}"  href="{{ route('pengumuman.index') }}">Pengumuman</a></li>
         @hasanyrole(['admin', 'panitia', 'bendahara'])
         <li><a class="nav-link scrollto" href="{{ route('admin.dashboard') }}">Dashboard Admin</a></li>
         @endhasrole
